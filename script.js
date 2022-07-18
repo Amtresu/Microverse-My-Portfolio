077069338aeb57922cb795d9cc9d28edcb2b4895
Amtresu <conor.follette@gmail.com>
2022-07-19 00:07:50 +0900

    fixing linter errors

--- script.js
+++ script.js
@@ -1,26 +1,26 @@
-let hamburgerMenu = document.getElementById("menu_icon");
-let popupWindow = document.getElementById("popup_window");
-let closePopupWindow = document.getElementById("close_popup");
-let toggleMenu = false;
-let closePopupWindowFunction = false;
+const hamburgerMenu = document.getElementById('menu_icon');
+const popupWindow = document.getElementById('popup_window');
+const closePopupWindow = document.getElementById('close_popup');
+const toggleMenu = false;
+const closePopupWindowFunction = false;
 
-hamburgerMenu.addEventListener("click", function() {
-    if(toggleMenu === false){
-        popupWindow.style.visibility = "visible";
+hamburgerMenu.addEventListener('click', function (openMenu) {
+  if(toggleMenu === false){
+     popupWindow.style.visibility = 'visible';
     }
 });
 
   
-closePopupWindow.addEventListener("click", function(){
+closePopupWindow.addEventListener('click', function (closeMenu){
     if(toggleMenu === false){
-        popupWindow.style.visibility = "hidden"; 
+        popupWindow.style.visibility = 'hidden'; 
     }
 });
 
-let navMenuButton= document.querySelector('.popup_menu');
+const navMenuButton= document.querySelector('.popup_menu');
 
-navMenuButton.addEventListener("click", function(){
+navMenuButton.addEventListener('click', function (closeMenu){
     if(toggleMenu=== false){
-        popupWindow.style.visibility = "hidden";
+        popupWindow.style.visibility = 'hidden';
     }
 });
