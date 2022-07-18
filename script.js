459b4300e88a63a6910226d5a16373b5b5126327
Amtresu <conor.follette@gmail.com>
2022-07-19 00:13:12 +0900

    fixing linter errors

--- script.js
+++ script.js
@@ -5,22 +5,22 @@ const toggleMenu = false;
 const closePopupWindowFunction = false;
 
 hamburgerMenu.addEventListener('click', function (openMenu) {
-  if(toggleMenu === false){
-     popupWindow.style.visibility = 'visible';
+    if (toggleMenu === false) {
+        popupWindow.style.visibility = 'visible';
     }
 });
 
-  
-closePopupWindow.addEventListener('click', function (closeMenu){
-    if(toggleMenu === false){
-        popupWindow.style.visibility = 'hidden'; 
+
+closePopupWindow.addEventListener('click', function (closeMenu) {
+    if (toggleMenu === false) {
+        popupWindow.style.visibility = 'hidden';
     }
 });
 
-const navMenuButton= document.querySelector('.popup_menu');
+const navMenuButton = document.querySelector('.popup_menu');
 
-navMenuButton.addEventListener('click', function (closeMenu){
-    if(toggleMenu=== false){
+navMenuButton.addEventListener('click', function (closeMenu) {
+    if (toggleMenu === false) {
         popupWindow.style.visibility = 'hidden';
     }
 });
