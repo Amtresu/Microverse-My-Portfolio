a03e4d641839466f661566db0258182f60c5a0d3
Amtresu <conor.follette@gmail.com>
2022-07-20 21:21:31 +0900

    updating script

--- script.js
+++ script.js
@@ -23,178 +23,109 @@ navMenuButton.addEventListener('click', () => {
   }
 })
 
-const worksPopup = {
-  name: ['Tonic', 'Multi Post', 'Facebook 360', 'Uber Navigation'],
-  details: ['CANOPY', 'back end dev', '2015'],
-  description: [`Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has
-  been the industry's standard dummy text ever since the 1500s, when an unknown printer took a
-  galley of type and scrambled it to make a type specimen book. It has survived not only five
-  centuries, but also the leap into electronic typesetting, remaining essent,`, 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the relea', 'Hello There world'],
-  img: ['img/tonic.png', 'img/multipoststories.png', 'img/popup_snapshot.svg '],
-  languages: ['html', 'css', 'javascript', 'github', 'ruby', 'bootstrap'],
-  seeSource: 'https: //www.google.com/',
-  seeLive: 'https://www.google.com/'
-}
-const seeProjectTonic = document.querySelector('.see_project_tonic')
-const PopupMobile = document.getElementById('works_popup_mobile')
-const PopupDesktop = document.getElementById('works_popup_desktop')
-const closePopup = document.getElementById('close_project_popup')
-const closePopupDesktop = document.getElementById('close_project_popup_desktop')
-seeProjectTonic.addEventListener('click', () => {
-  if (toggleMenu === false) {
-    PopupMobile.style.visibility = 'visible'
-    document.querySelector('.project1_title_popup').innerHTML = worksPopup.name[0]
-    document.querySelector('.popup_canopy').innerHTML = worksPopup.details[0]
-    document.querySelector('.popup_li1').innerHTML = worksPopup.details[1]
-    document.querySelector('.popup_li2').innerHTML = worksPopup.details[2]
-    document.querySelector('.popup_img').src = worksPopup.img[0]
-    document.querySelector('.popup_description').innerHTML = worksPopup.description[0]
-    document.querySelector('.language_descriptor1').innerHTML = worksPopup.languages[0]
-    document.querySelector('.language_descriptor2').innerHTML = worksPopup.languages[1]
-    document.querySelector('.language_descriptor3').innerHTML = worksPopup.languages[2]
+const worksPopup = [
+  {
+     id: 1,
+     name: 'Tonic',
+     img: '',
+     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release',
+     languages: ['html', 'css', 'javascript'],
+     live: 'https://www.google.com',
+     source: 'https://www.google.com'
+  },
+  {
+     id: 2,
+     name: 'Multi-Post Stories',
+     img: '',
+     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release',
+     languages: ['html', 'css', 'Ruby on rails', 'javascript'],
+     live: 'https://www.google.com',
+     source: 'https://www.google.com'
+  },
+  {
+     id: 3,
+     name: 'Facebook 360',
+     img: '',
+     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release',
+     languages: ['html', 'css', 'Ruby on rails', 'javascript'],
+     liveLink: 'https://www.google.com',
+     sourceLink: 'https://www.google.com'
+  },
+  {
+     id: 4,
+     name: 'Uber',
+     img: '',
+     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release',
+     languages: ['html', 'css', 'javascript', 'Ruby on rails'],
+     live: 'https://www.google.com/',
+     source: 'https://www.google.com/'
   }
-})
+]
 
-closePopup.addEventListener('click', () => {
+const popup = `<aside id="works_popup_desktop">
+<div class="project1_popup">
+    <div class="project1_popup_container">
+        <div class="popup_header">
+            <h3 class="project1_title_popup_desktop">${worksPopup.name}</h3>
+            <div>
+                <button type="button" id="close_project_popup_desktop">X</button>
+            </div>
+        </div>
+        <ul class="tonic_list">
+            <li class="popup_canopy_desktop canopy"></li>
+            <li> <img class="counter" src="img/Counter.png" alt="counter" /> </li>
+            <li class="popup_li1_desktop"></li>
+            <li> <img class="counter" src="img/Counter.png" alt="counter" /></li>
+            <li class="popup_li2_desktop"></li>
+        </ul>
+        <div class="project1_img">
+            <img src="${worksPopup.img}" alt="multi-post-stories" class="popup_img_desktop" />
+        </div>
+
+        <div class="desktop_popup_positioner">
+            <div class="popup_description_desktop_container">
+                <p class="popup_description_desktop">${worksPopup.description}</p>
+            </div>
+
+            <div>
+                <div class="small_button_project1_popup">
+                    <ul class="button_list">
+                        <li class="language_descriptor language_descriptor1_desktop">${worksPopup.languages}</li>
+                        <li class="language_descriptor language_descriptor2_desktop">${worksPopup.languages}</li>
+                        <li class="language_descriptor
+                    language_descriptor3_desktop">${worksPopup.languages}</li>
+                    </ul>
+                    <ul class="button_list">
+                        <li class="language_descriptor
+                    language_descriptor4_desktop">${worksPopup.languages}</li>
+                        <li class="language_descriptor
+                    language_descriptor5_desktop">${worksPopup.languages}</li>
+                        <li class="language_descriptor
+                    language_descriptor6_desktop">${worksPopup.languages}</li>
+                    </ul>
+                </div>
+
+                <img src="img/mobile_popup_seperator.png" alt="seperator" class="mobile_popup_seperator" />
+                <div class="project1_popup_buttons">
+                    <div class="see_live_container">
+                        <button class="see_project_mobile_popup1" type="button">See live <img
+                                src="img/see_live.png" alt="see_live_icon"></button>
+                    </div>
+                    <div class="see_source_container">
+                        <button class="see_project_mobile_popup2" type="button">See Source <img
+                                src="img/see_source.png" alt="github_logo"></button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</aside>`
+
+const seeProject = document.getElementById('see_project')
+
+seeProject.addEventListener('click', () => {
   if (toggleMenu === false) {
-    PopupMobile.style.visibility = 'hidden'
-  }
-})
-closePopupDesktop.addEventListener('click', () => {
-  if (toggleMenu === false) {
-    PopupDesktop.style.visibility = 'hidden'
-  }
-})
-
-const seeProjectTonic2 = document.querySelector('.see_project_tonic2')
-
-seeProjectTonic2.addEventListener('click', () => {
-  if (toggleMenu === false) {
-    PopupMobile.style.visibility = 'visible'
-    document.querySelector('.project1_title_popup').innerHTML = worksPopup.name[0]
-    document.querySelector('.popup_canopy').innerHTML = worksPopup.details[0]
-    document.querySelector('.popup_li1').innerHTML = worksPopup.details[1]
-    document.querySelector('.popup_li2').innerHTML = worksPopup.details[2]
-    document.querySelector('.popup_img').src = worksPopup.img[0]
-    document.querySelector('.popup_description').innerHTML = worksPopup.description[0]
-    document.querySelector('.language_descriptor1').innerHTML = worksPopup.languages[0]
-    document.querySelector('.language_descriptor2').innerHTML = worksPopup.languages[1]
-    document.querySelector('.language_descriptor3').innerHTML = worksPopup.languages[2]
-  }
-})
-
-const seeProjectMulti = document.querySelector('.see_project_multipoststories')
-
-seeProjectMulti.addEventListener('click', () => {
-  if (toggleMenu === false) {
-    PopupMobile.style.visibility = 'visible'
-    document.querySelector('.project1_title_popup').innerHTML = worksPopup.name[1]
-    document.querySelector('.popup_canopy').innerHTML = worksPopup.details[0]
-    document.querySelector('.popup_li1').innerHTML = worksPopup.details[1]
-    document.querySelector('.popup_li2').innerHTML = worksPopup.details[2]
-    document.querySelector('.popup_img').src = worksPopup.img[1]
-    document.querySelector('.popup_description').innerHTML = worksPopup.description[0]
-    document.querySelector('.language_descriptor1').innerHTML = worksPopup.languages[0]
-    document.querySelector('.language_descriptor2').innerHTML = worksPopup.languages[1]
-    document.querySelector('.language_descriptor3').innerHTML = worksPopup.languages[2]
-  }
-})
-
-const seeProjectMulti2 = document.querySelector('.see_project_multipoststories2')
-
-seeProjectMulti2.addEventListener('click', () => {
-  if (toggleMenu === false) {
-    PopupMobile.style.visibility = 'visible'
-    document.querySelector('.project1_title_popup').innerHTML = worksPopup.name[1]
-    document.querySelector('.popup_canopy').innerHTML = worksPopup.details[0]
-    document.querySelector('.popup_li1').innerHTML = worksPopup.details[1]
-    document.querySelector('.popup_li2').innerHTML = worksPopup.details[2]
-    document.querySelector('.popup_img').src = worksPopup.img[1]
-    document.querySelector('.popup_description').innerHTML = worksPopup.description[0]
-    document.querySelector('.language_descriptor1').innerHTML = worksPopup.languages[0]
-    document.querySelector('.language_descriptor2').innerHTML = worksPopup.languages[1]
-    document.querySelector('.language_descriptor3').innerHTML = worksPopup.languages[2]
-  }
-})
-
-// Desktop
-
-const seeProjectDesktop1 = document.querySelector('.see_project_tonic_desktop1')
-
-seeProjectDesktop1.addEventListener('click', () => {
-  if (toggleMenu === false) {
-    PopupDesktop.style.visibility = 'visible'
-    document.querySelector('.project1_title_popup_desktop').innerHTML = worksPopup.name[0]
-    document.querySelector('.popup_canopy_desktop').innerHTML = worksPopup.details[0]
-    document.querySelector('.popup_li1_desktop').innerHTML = worksPopup.details[1]
-    document.querySelector('.popup_li2_desktop').innerHTML = worksPopup.details[2]
-    document.querySelector('.popup_img_desktop').src = worksPopup.img[2]
-    document.querySelector('.popup_description_desktop').innerHTML = worksPopup.description[1]
-    document.querySelector('.language_descriptor1_desktop').innerHTML = worksPopup.languages[0]
-    document.querySelector('.language_descriptor2_desktop').innerHTML = worksPopup.languages[1]
-    document.querySelector('.language_descriptor3_desktop').innerHTML = worksPopup.languages[2]
-    document.querySelector('.language_descriptor4_desktop').innerHTML = worksPopup.languages[3]
-    document.querySelector('.language_descriptor5_desktop').innerHTML = worksPopup.languages[4]
-    document.querySelector('.language_descriptor6_desktop').innerHTML = worksPopup.languages[5]
-  }
-})
-
-const seeProjectDesktop2 = document.querySelector('.see_project_tonic_desktop2')
-
-seeProjectDesktop2.addEventListener('click', () => {
-  if (toggleMenu === false) {
-    PopupDesktop.style.visibility = 'visible'
-    document.querySelector('.project1_title_popup_desktop').innerHTML = worksPopup.name[1]
-    document.querySelector('.popup_canopy_desktop').innerHTML = worksPopup.details[0]
-    document.querySelector('.popup_li1_desktop').innerHTML = worksPopup.details[1]
-    document.querySelector('.popup_li2_desktop').innerHTML = worksPopup.details[2]
-    document.querySelector('.popup_img_desktop').src = worksPopup.img[2]
-    document.querySelector('.popup_description_desktop').innerHTML = worksPopup.description[1]
-    document.querySelector('.language_descriptor1_desktop').innerHTML = worksPopup.languages[0]
-    document.querySelector('.language_descriptor2_desktop').innerHTML = worksPopup.languages[1]
-    document.querySelector('.language_descriptor3_desktop').innerHTML = worksPopup.languages[2]
-    document.querySelector('.language_descriptor4_desktop').innerHTML = worksPopup.languages[3]
-    document.querySelector('.language_descriptor5_desktop').innerHTML = worksPopup.languages[4]
-    document.querySelector('.language_descriptor6_desktop').innerHTML = worksPopup.languages[5]
-  }
-})
-
-const seeProjectDesktop3 = document.querySelector('.see_project_tonic_desktop3')
-
-seeProjectDesktop3.addEventListener('click', () => {
-  if (toggleMenu === false) {
-    PopupDesktop.style.visibility = 'visible'
-    document.querySelector('.project1_title_popup_desktop').innerHTML = worksPopup.name[2]
-    document.querySelector('.popup_canopy_desktop').innerHTML = worksPopup.details[0]
-    document.querySelector('.popup_li1_desktop').innerHTML = worksPopup.details[1]
-    document.querySelector('.popup_li2_desktop').innerHTML = worksPopup.details[2]
-    document.querySelector('.popup_img_desktop').src = worksPopup.img[2]
-    document.querySelector('.popup_description_desktop').innerHTML = worksPopup.description[1]
-    document.querySelector('.language_descriptor1_desktop').innerHTML = worksPopup.languages[0]
-    document.querySelector('.language_descriptor2_desktop').innerHTML = worksPopup.languages[1]
-    document.querySelector('.language_descriptor3_desktop').innerHTML = worksPopup.languages[2]
-    document.querySelector('.language_descriptor4_desktop').innerHTML = worksPopup.languages[3]
-    document.querySelector('.language_descriptor5_desktop').innerHTML = worksPopup.languages[4]
-    document.querySelector('.language_descriptor6_desktop').innerHTML = worksPopup.languages[5]
-  }
-})
-
-const seeProjectDesktop4 = document.querySelector('.see_project_tonic_desktop4')
-
-seeProjectDesktop4.addEventListener('click', () => {
-  if (toggleMenu === false) {
-    PopupDesktop.style.visibility = 'visible'
-    document.querySelector('.project1_title_popup_desktop').innerHTML = worksPopup.name[3]
-    document.querySelector('.popup_canopy_desktop').innerHTML = worksPopup.details[0]
-    document.querySelector('.popup_li1_desktop').innerHTML = worksPopup.details[1]
-    document.querySelector('.popup_li2_desktop').innerHTML = worksPopup.details[2]
-    document.querySelector('.popup_img_desktop').src = worksPopup.img[2]
-    document.querySelector('.popup_description_desktop').innerHTML = worksPopup.description[1]
-    document.querySelector('.language_descriptor1_desktop').innerHTML = worksPopup.languages[0]
-    document.querySelector('.language_descriptor2_desktop').innerHTML = worksPopup.languages[1]
-    document.querySelector('.language_descriptor3_desktop').innerHTML = worksPopup.languages[2]
-    document.querySelector('.language_descriptor4_desktop').innerHTML = worksPopup.languages[3]
-    document.querySelector('.language_descriptor5_desktop').innerHTML = worksPopup.languages[4]
-    document.querySelector('.language_descriptor6_desktop').innerHTML = worksPopup.languages[5]
+    document.getElementById('post').innerHTML = popup
   }
 })
