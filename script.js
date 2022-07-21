7e5a287a68d34eddac555f23a8a762a304188838
Amtresu <conor.follette@gmail.com>
2022-07-21 13:55:12 +0900

    fixing image url

--- script.js
+++ script.js
@@ -66,7 +66,7 @@ const popupInfo = [
     },
     screenshot: 'img/snapshot3.svg',
     popupScreenshot: 'img/popup_snapshot.svg',
-    mobileScreenshot: 'img/Tonic2.png',
+    mobileScreenshot: 'img/tonic2.png',
     description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the releorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum han printer took a galley of type and scrambled it 1960s with the releawn printer took a galley of type and scrambled it 1960s with the releaLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s with the release',
     languages: ['html', 'css', 'javascript'],
     liveLink: 'https://www.google.com/',
